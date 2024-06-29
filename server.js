1b99aaba51b4cbdffa2f73537c9c2cbd58477e40
Sarvex Jatasra <sarvex.jatasra@gmail.com>
2024-06-29 05:59:29 +0530

    Cleanup Provided

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.set('views', path.join(__dirname, './views'))
 
 app.use(express.static(path.join(__dirname, './static')))
 
-app.use('/', routes)
+app.use('/', routes())
 
 app.listen(port, () => {
   console.log(`express server listening on port ${port}`)
